088a23b9163bfffac70c557456b71061d0012074
Belsky <dbelsky@slurmfe-n01.ipa.srcinc.com>
2022-02-03 21:22:43 -0500

    Addressed some border cases. Also it can handle multiple instructions now

--- pluto.py
+++ pluto.py
@@ -1,10 +1,3 @@
-
-line="paddce $0 23 34"
-
-x = line.split()
-
-command=x[0]
-
 opcodes = { 
 # Integer operations
 "padd"        : 0,
@@ -62,62 +55,73 @@ registers = [0] * 64
 # Data section
 data = [0] * 2048
 
-ALU_Reg = ["padd", "psub", "pmult", "pdiv", "pfadd", "pfsub", "pfmult", "pfdiv"]
-if x[0] in ALU_Reg :
-   opcode = opcodes[x[0]]
-   result = int(x[1][1:]) << 6
-   source1 = int(x[2][1:]) << 12
-   source2 = int(x[3][1:]) << 18
-   elements = int(x[4]) << 24
-   print("Hex Format: "+hex(opcode | result | source1 | source2 | elements)) 
-   print("Bin Format: "+bin(opcode | result | source1 | source2 | elements)) 
-   
-ALU_Const = ["paddc", "psubc", "pmultc", "pdivc", "pfaddc", "pfsubc", "pfmultc", "pfdivc"]
-if x[0] in ALU_Const :
-   opcode = opcodes[x[0]]
-   result = int(x[1][1:]) << 6
-   source1 = int(x[2][1:]) << 12
-   constant = int(x[3]) << 18
-   elements = int(x[4]) << 26
-   print("Hex Format: "+hex(opcode | result | source1 | constant | elements)) 
-   print("Bin Format: "+bin(opcode | result | source1 | constant | elements)) 
-   
-ALU_Ext_Const = ["paddce", "psubce", "pmultce", "pdivce", "pfaddce", "pfsubce", "pfmultce", "pfdivce"]
-if x[0] in ALU_Ext_Const :
-   opcode = opcodes[x[0]]
-   
-   if x[0] == "paddce" or x[0] == "psubce" or x[0] == "pmultce" or x[0] == "pdivce":
-      result = int(x[1][1:]) # strip $
-      constant = int(x[2])
-   elements = int(x[3])
+program=["paddce $0 2 64", "pmultce $0 2 32", "pmultce $0 2 16", "pmultce $0 2 8", "pmultce $0 2 4", "pmultce $0 2 2", "pmultce $0 2 1"]
 
-   # Form 32bit code
-   code_32bit = opcode | result << 6 | constant << 12 | elements << 26
-   print("Hex Format: "+hex(code_32bit))
-   print("Bin Format: "+bin(code_32bit)) 
-   
-   # Move constant into registers
-   for i in range(0, elements):
-      if x[0] == "paddce" or x[0] == "pfaddce": 
-         registers[result+i] = registers[result+i] + constant
-      elif x[0] == "psubce" or x[0] == "pfsubce":
-         registers[result+i] = registers[result+i] - constant
-      elif x[0] == "pmultce" or x[0] == "pfmultce":
-         registers[result+i] = registers[result+i] * constant
-      elif x[0] == "pdivce" or x[0] == "pfdivce":
-         registers[result+i] = registers[result+i] / constant
+count = 0
+for code in program:
+   print("\n--------------------Running Instruction #",count,'----------------------------------\n')
+   x = code.split()
 
-print()
-ALU_Reg_Source2_Const = ["paddreg", "psubreg", "pmultreg", "pdivreg", "pfaddreg", "pfsubreg", "pfmultreg", "pfdivreg"]
-if x[0] in ALU_Reg_Source2_Const :
-   opcode = opcodes[x[0]]
-   result = int(x[1][1:]) << 6
-   source1 = int(x[2][1:]) << 12
-   source2_constant = int(x[3][1:]) << 18
-   elements = int(x[4]) << 24
-   print("Hex Format: "+hex(opcode | result | source1 | source2_constant | elements)) 
-   print("Bin Format: "+hex(opcode | result | source1 | source2_constant | elements)) 
-   
-print("Registers: ",registers)
+   command=x[0]
+
+   ALU_Reg = ["padd", "psub", "pmult", "pdiv", "pfadd", "pfsub", "pfmult", "pfdiv"]
+   if x[0] in ALU_Reg :
+      opcode = opcodes[x[0]]
+      result = int(x[1][1:]) << 6
+      source1 = int(x[2][1:]) << 12
+      source2 = int(x[3][1:]) << 18
+      elements = int(x[4]) << 24
+      print("Hex Format: "+hex(opcode | result | source1 | source2 | elements)) 
+      print("Bin Format: "+bin(opcode | result | source1 | source2 | elements)) 
+      
+   ALU_Const = ["paddc", "psubc", "pmultc", "pdivc", "pfaddc", "pfsubc", "pfmultc", "pfdivc"]
+   if x[0] in ALU_Const :
+      opcode = opcodes[x[0]]
+      result = int(x[1][1:]) << 6
+      source1 = int(x[2][1:]) << 12
+      constant = int(x[3]) << 18
+      elements = int(x[4]) << 26
+      print("Hex Format: "+hex(opcode | result | source1 | constant | elements)) 
+      print("Bin Format: "+bin(opcode | result | source1 | constant | elements)) 
+      
+   ALU_Ext_Const = ["paddce", "psubce", "pmultce", "pdivce", "pfaddce", "pfsubce", "pfmultce", "pfdivce"]
+   if x[0] in ALU_Ext_Const :
+      opcode = opcodes[x[0]]
+      
+      if x[0] == "paddce" or x[0] == "psubce" or x[0] == "pmultce" or x[0] == "pdivce":
+         result = int(x[1][1:]) % 2**6 # strip $ and treat as only 6 bits. 
+         constant = int(x[2]) % 2**14 # treat as only 14 bits. 
+      # TODO Add floating point. 
+      elements = (int(x[3])-1) % 2**6
+      
+      # Form 32bit code
+      code_32bit = opcode | result << 6 | constant << 12 | elements << 26
+      print("Hex Format: "+hex(code_32bit))
+      print("Bin Format: "+bin(code_32bit)) 
+      
+      # Move constant into registers
+      for i in range(0, elements):
+         if x[0] == "paddce" or x[0] == "pfaddce": 
+            registers[result+i] = registers[result+i] + constant
+         elif x[0] == "psubce" or x[0] == "pfsubce":
+            registers[result+i] = registers[result+i] - constant
+         elif x[0] == "pmultce" or x[0] == "pfmultce":
+            registers[result+i] = registers[result+i] * constant
+         elif x[0] == "pdivce" or x[0] == "pfdivce":
+            registers[result+i] = int(registers[result+i] / constant)
+
+   print()
+   ALU_Reg_Source2_Const = ["paddreg", "psubreg", "pmultreg", "pdivreg", "pfaddreg", "pfsubreg", "pfmultreg", "pfdivreg"]
+   if x[0] in ALU_Reg_Source2_Const :
+      opcode = opcodes[x[0]]
+      result = int(x[1][1:]) << 6
+      source1 = int(x[2][1:]) << 12
+      source2_constant = int(x[3][1:]) << 18
+      elements = int(x[4]) << 24
+      print("Hex Format: "+hex(opcode | result | source1 | source2_constant | elements)) 
+      print("Bin Format: "+hex(opcode | result | source1 | source2_constant | elements)) 
+      
+   print("Registers: ",registers)
+   count = count + 1
 
 
