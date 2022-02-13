5df1de87c4c90a15e0daca0dba8636c7ee113113
derekbelsky102 <db9134@cs.ship.com>
2022-02-13 18:48:46 -0500

    removed program list and fixed bug with formatting

--- pluto.py
+++ pluto.py
@@ -72,38 +72,6 @@ data = [0] * 2048
 # To set all 64 register to255-2^16-1, it takes two instructions
 # To set all 64 register to 2^16-1 - 2^32-1, it takes four instructions
 
-program=[
-"pset8 $0 $0 2 64",
-"pset16lwr $0 2048 32",
-"pset16lwr $32 2048 32",
-"padd $0 $0 $0 64",
-"pmult $0 $0 $0 64",
-"pset16lwr $0 500077 32",
-"pset16upr $0 500077 32",
-"pset16lwr $32 500077 32",
-"pset16upr $32 500077 32",
-"pset8 $63 $0 0 1",
-"psw $0 $63 0 63",
-"pset8 $0 $0 2 64",
-"pset16lwr $62 0xAAAAAAAA 1",
-"pset16upr $62 0xAAAAAAAA 1",
-"pset16lwr $63 0xAAAAAAAA 1",
-"pset16upr $63 0xAAAAAAAA 1",
-"pctl 1",
-"padd8 $0 $0 8 0",
-"pctl 0",
-"padd8 $0 $0 8 32",
-"pset8 $63 $0 0 1",
-"psw $0 $63 63 62",
-"plw $0 $63 0 62",
-"padd8 $62 $0 0 2",
-"pand16lwr $0 0x00008000 32",
-"pand16upr $0 0x00008000 32",
-"por16lwr $0 0x0007A16D 32",
-"por16upr $0 0x0007A16D 32",
-"pnot $0 32"
-]
-
 file="program1.asm"
 mode=int(input("Enter mode. \n 1: Run All \n 2: Step Through\n"))
 
@@ -113,7 +81,8 @@ with open(file, 'r') as program:
     for code in program:
        # This printing scheme could be cleaner. If there is time this can be improved. 
        print("\n---------------------- Running Instruction #",count,"---------------------------")
-       print("----------------------",code,'-'*(50-int(len(code)))+'-'*(len(str(count))-1),'\n')
+       strippedNewlineCode = code.replace('\n','')
+       print("----------------------",strippedNewlineCode,'-'*(50-int(len(code)))+'-'*(len(str(count))),'\n')
        x = code.split()
        
        result=0
