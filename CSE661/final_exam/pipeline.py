3fb2b36c38ee4a76aa77ab737853a552a0821432
Belsky <dbelsky@syr-slurmc-n43.ipa.srcinc.com>
2022-03-22 19:58:44 -0400

    Updated so it does solution 1 and 2 in assignment

--- CSE661/final_exam/pipeline.py
+++ CSE661/final_exam/pipeline.py
@@ -2,23 +2,72 @@ potentialHazards = ["NA","NA"]
 
 file="program1.asm"
 spaces=""
+insertStall=0
 count=1
+previousLines=0
+solution_type=input("Enter Solution Type 1, 2 or 3 ")
 with open(file, 'r') as program:
     for code in program:
       
       x = code.split()
       
-      result =  (x[1][1:])
-      source1 = (x[2][1:])
-      source2 = (x[3][1:])
-      if source1 in potentialHazards:
-         print("Data dependency on line ",count, "Register ",source1)
-      if source2 in potentialHazards:
-         print("Data dependency on line ",count, "Register ",source2)
+      if x[0] == "add" or x[0] == "sub":
+         result =  (x[1][1:])
+         source1 = (x[2][1:])
+         source2 = (x[3][1:])
+      elif x[0] == "lw":
+         result =  (x[1][1:])
+         startIndex=x[2].index("$")+1
+         endIndex=x[2].index(")")
+         source1 = (x[2][startIndex:endIndex])
+         source2=""
+      elif x[0] == "sw":
+         result=""
+         source1 =  (x[1][1:])
+         startIndex=x[2].index("$")+1
+         endIndex=x[2].index(")")
+         source2 = (x[2][startIndex:endIndex])
+      else:
+         result=""
+         source1=""
+         source2=""
+         print("Invalid command")
+      
+      source1Flag=0
+      if source1 in potentialHazards or source2 in potentialHazards:
+         if source1 == potentialHazards[1] or source2 == potentialHazards[1]:
+            if solution_type=='1':
+               if source1 in potentialHazards:
+                  print("Data dependency on line "+str(count)+" with register "+source1+" from line "+str(count-1))
+               if source2 in potentialHazards:
+                  print("Data dependency on line "+str(count)+" with register "+source2+" from line "+str(count-1))
+            elif solution_type=='2':
+               print(spaces+"F S S D X M W")
+               spaces=spaces+"    "
+               # There can be no dependency after two stalls
+               potentialHazards[0]="NA"
+               potentialHazards[1]="NA"
+         elif source1 == potentialHazards[0] or source2 == potentialHazards[0]:
+            if solution_type=='1':
+               if source1 in potentialHazards:
+                  print("Data dependency on line "+str(count)+" with register "+source1+" from line "+str(count-2))
+               if source2 in potentialHazards:
+                  print("Data dependency on line "+str(count)+" with register "+source2+" from line "+str(count-2))
+            elif solution_type=='2':
+               print(spaces+"F S D X M W")
+               spaces=spaces+"  "
+               # With a single stall added, this result can't be dependency anymore. 
+               del potentialHazards[0]
+               potentialHazards.append("NA")
+         insertStall=1
+      
       potentialHazards.append(result)
       del potentialHazards[0] 
-      spaces=spaces+"    "  
+      if solution_type=='2' and insertStall==0:
+         print(spaces+"F D X M W")
+      spaces=spaces+"  "  
+      insertStall=0
       count = count + 1
-       
+   
 
 
--- CSE661/final_exam/program1.asm
+++ CSE661/final_exam/program1.asm
@@ -1,3 +1,8 @@
-add $0 $1 $2
-add $2 $0 $2
-add $0 $1 $2
+add $r0 $r1 $r2
+add $r1 $r3 $r3
+add $r0 $r0 $r2
+lw $r0 24($r1)
+sw $r0 8($r1)
+add $r0 $r0 $r1
+add $r0 $r1 $r1
+sub $r0 $r1 $r0
